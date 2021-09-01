d1b6450eddbfa750349d8b543d8eeab7bd31b242
francisco-solis99 <franciscosoliscss@gmail.com>
2021-08-31 19:37:50 -0500

    [Add] interactive's template

--- js/main.js
+++ js/main.js
@@ -2,8 +2,27 @@
 'use strict'
 const menu = document.querySelector('.header__menu-container');
 const burgerIcon = document.querySelector('.header__burger');
+const shorterBtn = document.querySelector('#shorten-btn');
+
+
+
+
+
+
 
 // Event listener to show and hide the menu with burger icon
 burgerIcon.addEventListener('click', () => {
     menu.classList.toggle('header__menu-active');
 });
+
+shorterBtn.addEventListener('click',  getNewResult);
+
+function getNewResult() {
+  const sectionResult = document.querySelector('.results');
+  const template = document.querySelector("template");
+  const clon = template.content.cloneNode(true);
+  sectionResult.appendChild(clon);
+}
+
+
+// template
